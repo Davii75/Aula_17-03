2ed705c6aea85fcd8e1d607eabbad67ede9120ab
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-17 09:07:24 -0300

    Finaliza aula 17/03 (parte 1)

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+// string nome = "Davi";
+// string sobrenome = "Oliveira";
+
+// Console.Write("Meu nome é ");
+// Console.Write(nome);
+// Console.Write(" ");
+// Console.Write(sobrenome);
+
+// Tipo de concatenação = Console.WriteLine("Meu nome é " + nome + " " + sobrenome);
+// Interpolação = Console.WriteLine($"Meu nome é {nome} {sobrenome}");
+// Interpolação >>> Concatenação padrão
+
+// - PARTE 2 - 
+
+// Console.Clear();
+
+// string nome;
+// string sobrenome;
+
+// Console.WriteLine("Nome".PadRight(11, '.')); -> comando que move para a direita 
+// (caso queira mover pra esquerda => PadLeft)
+// nome = Console.ReadLine();
+
+// Console.WriteLine("Sobrenome".PadRight(11, '.'));
+// sobrenome = Console.ReadLine();
+
+// Console.WriteLine($"Seu nome é {nome} {sobrenome}");
+
+// - PARTE 3 - 
+
+// Console.WriteLine("fhjdsfhsdofsdfsdfsdiofjsdif".Remove(5);
+// No caso, ele remove as letras até a da quinta posição (remove 5)
+// Console.WriteLine("bala".Replace("a", "o"));
+// Troca as letras "a" por "o" na palavra escrita (bala)
+
+// - PARTE 4 - 
+
+// string oQueEuAcho;
+//string textoCensurado;
+// onsole.Write("O que você acha de alunos que falam durante a aula? ");
+//oQueEuAcho = Console.ReadLine();
+//textoCensurado = oQueEuAcho
+//                  .Replace("chato", "#@$%*!&")
+//                  .Replace("chata", "#@$%*!&")
+//                  .Replace("bobo", "#@$%*!&")
+//                  .Replace("boba", "#@$%*!&")
+//                  .Replace("feio", "#@$%*!&")
+//                  .Replace("feia", "#@$%*!&")
+//                  ;
+//Console.WriteLine($"Texto original: {oQueEuAcho}");
+//Console.WriteLine($"Texto censurado: {textoCensurado}");
